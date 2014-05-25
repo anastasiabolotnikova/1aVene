7d2c37f4cbd563833d1aabfc6d6695dfe9368304
Anton <prokopov@hotmail.com>
2014-05-25 17:58:03 +0300

    Main class added

---
+++ Main.java
@@ -0,0 +1,29 @@
+/**
+ * Created by Admin on 25.05.2014.
+ */
+import org.antlr.v4.runtime.tree.ParseTree;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main (String args[]) throws FileNotFoundException {
+
+        String fileName = args[0];
+        String content = getFileContent(fileName);
+
+        ParseTree tree;
+        tree = Interpretator.createParseTree(content);
+        Interpretator.evaluate(tree);
+    }
+
+    public static String getFileContent(String filename) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File(filename));
+        String expr = "";
+        while(sc.hasNextLine()) {
+            expr += sc.nextLine()+"\n";
+        }
+        sc.close();
+        return expr;
+    }
+}
